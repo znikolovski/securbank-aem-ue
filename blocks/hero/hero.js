9ec1fbc90f0fd67667b3ca1bc8a86e1f12ab0f80
Mark Szulc <mszulc@adobe.com>
2024-02-13 20:00:56 +1100

    Hero styling

---
+++ blocks/hero/hero.js
@@ -0,0 +1,12 @@
+export default async function decorate(block) {
+    let row = block.firstElementChild;
+    const bg = row.querySelector('picture');
+    block.append(bg);
+    row.remove();
+    const bgP = block.closest('p');
+    if (bgP) bgP.remove();
+  
+    row = block.firstElementChild;
+    row.classList.add('hero-body');
+  }
+  
