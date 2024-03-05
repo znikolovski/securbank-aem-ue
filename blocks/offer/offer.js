10662369a072813414739650290216c587e3530c
Mark Szulc <mszulc@adobe.com>
2024-03-06 09:52:41 +1100

    actually put the author url in correctly.

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
   const aempublishurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
-  const aemauthorurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
+  const aemauthorurl = 'https://author-p123917-e1220159.adobeaemcloud.com';
   const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
   const offerid = block.querySelector(':scope div:nth-child(1) > div a').innerHTML;
   const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML;
+
   const url = window.location && window.location.origin && window.location.origin.includes('author') ?
   `${aemauthorurl}${persistedquery};path=${offerid};variation=${variationname};ts=${Math.random() * 1000}` :
   `${aempublishurl}${persistedquery};path=${offerid};variation=${variationname};ts=${Math.random() * 1000}`;
