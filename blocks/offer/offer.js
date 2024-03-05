a324228f8ebcaa8d464156b6c5e31935bf79b713
Mark Szulc <mszulc@adobe.com>
2024-03-06 10:37:03 +1100

    temp use of Publish

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
   const aempublishurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
-  const aemauthorurl = 'https://author-p123917-e1220159.adobeaemcloud.com';
+  const aemauthorurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
   const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
   const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
   const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
@@ -11,7 +11,7 @@ export default async function decorate(block) {
   `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
   const options = {credentials: "include"};
 
-  console.log(url);
+  console.log(url); //https://author-p123917-e1220159.adobeaemcloud.com/graphql/execute.json/securbank/OfferByPath;path=/content/dam/securbank/en/offers/997;variation=main;ts=172.03956935404463
 
   const cfReq = await fetch(url, options)
     .then((response) => response.json())
