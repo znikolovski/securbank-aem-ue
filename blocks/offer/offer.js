1b70b46691c298f1341eccebdaefa755632104e9
Mark Szulc <mszulc@adobe.com>
2024-03-06 20:51:26 +1100

    switch to authorURL

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
   const aempublishurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
-  const aemauthorurl = 'https://publish-p123917-e1220159.adobeaemcloud.com'; // Temp using Publish URL
+  const aemauthorurl = 'https://author-p123917-e1220159.adobeaemcloud.com';
   const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
   const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
   const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
