2d4450927cefec914e0dac35e91f0d8b90aeaea0
Mark Szulc <mszulc@adobe.com>
2024-03-07 23:31:02 +1100

    leave p tag

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,10 +1,13 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default async function decorate(block) {
   let row = block.firstElementChild;
   const bg = row.querySelector('picture');
   block.append(bg);
   row.remove();
   const bgP = block.closest('p');
-  if (bgP) bgP.remove();
+
 
   row = block.firstElementChild;
   row.classList.add('hero-body');
