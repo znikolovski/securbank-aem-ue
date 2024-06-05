a0e1aa36f31ab68cb705f5ae093d7e679e3beaf9
Zoran Nikolovski <nikolovs@adobe.com>
2024-06-06 08:57:39 +1000

    Update offer.js

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -29,7 +29,7 @@ export default async function decorate(block) {
   block.innerHTML = `
   <div class='banner-content' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
       <div data-aue-prop="heroImage" data-aue-type="media" class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${aempublishurl+cfReq.heroImage._dynamicUrl});">
-          <p data-aue-prop="pretitle" class='pretitle'>${cfReq.pretitle}</p>
+          <p data-aue-prop="pretitle" data-aue-type="text" class='pretitle'>${cfReq.pretitle}</p>
           <p data-aue-prop="headline" data-aue-type="text" class='headline'>${cfReq.headline}</p>
           <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.detail.plaintext}</p>
       </div>
