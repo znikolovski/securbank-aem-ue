7b2fc183acf13698eb3463b2c34c51cee3157b81
Mark Szulc <mszulc@adobe.com>
2024-07-16 20:37:10 +1000

    style complete and initial api call

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,4 +1,22 @@
 export default async function decorate(block) {
-    block.innerHTML = `<h4>Frequenty Asked Questions</h4>`;
-  }
-å
+
+  const url = 'https://publish-p55117-e571178.adobeaemcloud.com/graphql/execute.json/securbank/FAQListbyTag;tag=transactions';
+  const options = {};
+  const faq = await fetch(url, options);
+  const index = await faq.json();
+  console.log(index.data.faqList.items);
+
+  block.innerHTML = `
+                <h4 class='sectionHeading'>Frequently Asked Questions</h4>
+                <ul class="faqList">
+                <li>
+                  <details class="faqDetails">    
+                                  <summary class="faqHeading">
+                                      <span data-aue-prop="question" data-aue-type="text" >Question</span>
+                                      <b></b>
+                                  </summary>
+                                  <div data-aue-prop="answer" data-aue-type="richtext" class="faqDescription">Answer</div>
+                              </details>
+                </li>
+                </ul>`;
+}
