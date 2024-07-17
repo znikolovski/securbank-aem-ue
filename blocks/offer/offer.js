7d7eab1c9d871176ecdc3abb27a1e2749d762952
Mark Szulc <mszulc@adobe.com>
2024-07-17 19:11:44 +1000

    Update AEM URL

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
-  const aempublishurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
-  const aemauthorurl = 'https://author-p123917-e1220159.adobeaemcloud.com';
+  const aempublishurl = 'https://publish-p55117-e571178.adobeaemcloud.com';
+  const aemauthorurl = 'https://author-p55117-e571178.adobeaemcloud.com';
   const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
   const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
   const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
@@ -10,8 +10,6 @@ export default async function decorate(block) {
     ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
     : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
   const options = { credentials: 'include' };
-
-  // console.log(url); //https://author-p123917-e1220159.adobeaemcloud.com/graphql/execute.json/securbank/OfferByPath;path=/content/dam/securbank/en/offers/997;variation=main;ts=172.03956935404463
 
   const cfReq = await fetch(url, options)
     .then((response) => response.json())
