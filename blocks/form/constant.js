8a297e887668ab60deb5c28cc666f62ac0de5d1b
Zoran Nikolovski <nikolovs@adobe.com>
2024-07-29 10:25:22 +1000

    Adding publish base url as per documentation

--- blocks/form/constant.js
+++ blocks/form/constant.js
@@ -19,7 +19,7 @@ export const defaultErrorMessages = {
 // eslint-disable-next-line no-useless-escape
 export const emailPattern = '([A-Za-z0-9][._]?)+[A-Za-z0-9]@[A-Za-z0-9]+(\.?[A-Za-z0-9]){2}\.([A-Za-z0-9]{2,4})?';
 
-let submitBaseUrl = '';
+let submitBaseUrl = 'https://publish-p115476-e1135027.adobeaemcloud.com';
 
 export function setSubmitBaseUrl(url) {
   submitBaseUrl = url;
