77cd39379ce2287778ce6fb92dd92dc1712ef9e4
Mark Szulc <mszulc@adobe.com>
2024-12-23 13:51:03 +1100

    Moved AEM endpoints to config file

---
+++ scripts/endpointconfig.js
@@ -0,0 +1,10 @@
+
+function getAEMPublish() {
+    return 'https://publish-p55117-e571178.adobeaemcloud.com';
+}
+
+function getAEMAuthor() {
+    return 'https://author-p55117-e571178.adobeaemcloud.com';
+}
+
+export { getAEMPublish, getAEMAuthor };
