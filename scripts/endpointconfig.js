b030854fc8b57b6bd0226ed72a6f413bdc1b363f
Zoran Nikolovski <nikolovs@adobe.com>
2025-01-22 11:02:48 +1100

    Update endpoints

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p137825-e1511252.adobeaemcloud.com';
+  return 'https://publish-p115476-e1135027.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p137825-e1511252.adobeaemcloud.com';
+  return 'https://author-p115476-e1135027.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
